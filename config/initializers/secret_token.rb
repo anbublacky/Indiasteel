f54d2a61cd4ebcdcd1cf0371f5a70ec6bde51319
Anbazhagan <anbublacky@gmail.com>
2012-01-26 12:42:23 +0530

    first push with devise configured

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IndiaSteel::Application.config.secret_token = '2d274b9c99d0b9abb024335c3e7b5f565bf01de9525b26b212c8c9bd8787dec5c13c0312efee9a3017a3399c453ef831de0ec1b9c967fb6adbdc811d8db8aba5'
